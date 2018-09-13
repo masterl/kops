c0b4eb4736db174b5ff6dbe326086e1b87edf600
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2018-09-13 02:36:02 -0700

    Adds normalize.css and stylus support

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,16 @@ const load_tasks = require('gulp-task-loader');
 load_tasks();
 
 const npm_tasks = [
-  'npm:copy_js'
+  'npm:copy_js',
+  'npm:css'
 ];
 
 const compile_tasks = [
-  'compile:index'
+  'compile:index',
+  'compile:stylus'
 ];
 
 gulp.task('default',npm_tasks.concat(compile_tasks), () => {
   gulp.watch('src/index.pug', ['compile:index']);
+  gulp.watch('src/stylus/**/*.styl', ['compile:stylus']);
 });
