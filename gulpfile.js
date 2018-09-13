6d1a090e178d243e8b772468ba3d859ca09b6170
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2018-09-13 01:41:02 -0700

    Adds pug compilation support and index.pug

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ const load_tasks = require('gulp-task-loader');
 
 load_tasks();
 
-gulp.task('default',['npm:copy_js'], () => {
+const npm_tasks = [
+  'npm:copy_js'
+];
+
+const compile_tasks = [
+  'compile:index'
+];
+
+gulp.task('default',npm_tasks.concat(compile_tasks), () => {
 
 });
