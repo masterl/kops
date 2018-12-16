06315583fd5f1bfc80340dcb09a2c031a70b918a
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2018-12-15 20:53:18 -0800

    Updated gulp to v4.0.0
    Also removed unused dependencies

--- gulpfile.js/serve-files.js
+++ gulpfile.js/serve-files.js
@@ -1,3 +1,4 @@
+const { series, parallel } = require('gulp');
 const chalk   = require('chalk');
 const express = require('express');
 const app     = express();
@@ -6,11 +7,20 @@ const build_dir = require('../build_utils/build_dir');
 
 const PORT = 4000;
 
-module.exports = () => {
+function serve_files (cb) {
   app.use(express.static(build_dir));
   app.listen(PORT, '0.0.0.0');
 
   console.log(chalk.bold.green(`\n\tServer running on localhost:${PORT}\n`));
+
+  cb();
 };
 
-module.exports.dependencies = ['compile:app-bundle', 'compile:index', 'compile:stylus'];
+module.exports = series(
+  parallel(
+    require('./compile-app-bundle'),
+    require('./compile-index'),
+    require('./compile-stylus')
+  ),
+  serve_files
+);
