0a575e0671199ef6ea4217389efaf809ad5ccc69
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2020-05-13 05:47:30 -0700

    Improved eslint configuration and applied on files

--- gulpfile.js/compile-app-bundle.js
+++ gulpfile.js/compile-app-bundle.js
@@ -3,49 +3,57 @@ const webpack       = require('webpack-stream');
 const path          = require('path');
 
 const build_dir = require('../build_utils/build_dir');
-const src_dir = require('../build_utils/src_dir');
+const src_dir   = require('../build_utils/src_dir');
 
 const dest_dir = path.join(build_dir, 'js');
 
 const mode = process.env.KOPS_DEV ? 'development' : 'production';
 
-function compile_app_bundle () {
-  return src('src/app/startup.js')
-    .pipe(webpack({
-      mode,
-      output: {
-        filename: 'bundle.js'
-      },
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            use:  {
-              loader:  'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
+function compile_app_bundle ()
+{
+    return src('src/app/startup.js')
+        .pipe(webpack({
+            mode,
+            output: {
+                filename: 'bundle.js'
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        use:  {
+                            loader:  'babel-loader',
+                            options: {
+                                presets: [
+                                    '@babel/preset-env'
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        test: /\.pug$/,
+                        use:  [
+                            'apply-loader',
+                            'pug-loader'
+                        ]
+                    },
+                    {
+                        test: /\.styl$/,
+                        use:  [
+                            'ignore-loader'
+                        ]
+                    }
+                ]
+            },
+            resolve: {
+                alias: {
+                    knockout: 'knockout/build/output/knockout-latest.js',
+                    page:     'page/page.js',
+                    app:      path.join(src_dir, 'app')
+                }
             }
-          },
-          {
-            test: /\.pug$/,
-            use:  ['apply-loader', 'pug-loader']
-          },
-          {
-            test: /\.styl$/,
-            use:  ['ignore-loader']
-          }
-        ]
-      },
-      resolve: {
-        alias: {
-          knockout: 'knockout/build/output/knockout-latest.js',
-          page:     'page/page.js',
-          app:      path.join(src_dir, 'app')
-        }
-      }
-    }))
-    .pipe(dest(dest_dir));
+        }))
+        .pipe(dest(dest_dir));
 }
 
 module.exports = compile_app_bundle;
--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,47 +1,59 @@
 const { series, parallel, watch } = require('gulp');
-const chalk = require('chalk');
+const chalk                       = require('chalk');
 
 const compile = {
-  index:        require('./compile-index'),
-  'app-bundle': require('./compile-app-bundle'),
-  stylus:       require('./compile-stylus')
+    index:        require('./compile-index'),
+    'app-bundle': require('./compile-app-bundle'),
+    stylus:       require('./compile-stylus')
 };
 
 const npm = {
-  css: require('./npm-css')
+    css: require('./npm-css')
 };
 
 const compile_tasks = [];
 const npm_tasks = [];
 
-for (const key in compile) {
-  compile_tasks.push(compile[key]);
+for (const key in compile)
+{
+    compile_tasks.push(compile[key]);
 }
 
-for (const key in npm) {
-  npm_tasks.push(npm[key]);
+for (const key in npm)
+{
+    npm_tasks.push(npm[key]);
 }
 
 const compile_and_npm_tasks = npm_tasks.concat(compile_tasks);
 
-function default_task (cb) {
-  console.log(chalk.blue('\n\tBuild generated!\n'));
+function default_task (cb)
+{
+    console.log(chalk.blue('\n\tBuild generated!\n'));
 
-  cb();
+    cb();
 }
 
-function startdev () {
-  watch('src/index.pug', compile.index);
-  watch(['src/stylus/**/*.styl', 'src/components/**/*.styl'], compile.stylus);
-  watch(['src/components/**/*.pug', 'src/**/*.js'], compile['app-bundle']);
+function startdev ()
+{
+    watch('src/index.pug', compile.index);
+    watch([
+        'src/stylus/**/*.styl',
+        'src/components/**/*.styl'
+    ], compile.stylus);
+    watch([
+        'src/components/**/*.pug',
+        'src/**/*.js'
+    ], compile['app-bundle']);
 }
 
-for (const key in compile) {
-  exports[`compile:${key}`] = compile[key];
+for (const key in compile)
+{
+    exports[`compile:${key}`] = compile[key];
 }
 
-for (const key in npm) {
-  exports[`npm:${key}`] = npm[key];
+for (const key in npm)
+{
+    exports[`npm:${key}`] = npm[key];
 }
 
 exports['serve-files'] = require('./serve-files');
@@ -49,9 +61,9 @@ exports['serve-files'] = require('./serve-files');
 exports.default = series(parallel(...compile_and_npm_tasks), default_task);
 
 exports.startdev = series(
-  parallel(...compile_and_npm_tasks),
-  parallel(
-    exports['serve-files'],
-    startdev
-  )
+    parallel(...compile_and_npm_tasks),
+    parallel(
+        exports['serve-files'],
+        startdev
+    )
 );
