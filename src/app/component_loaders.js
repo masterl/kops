7aadf085b3ff605da72ac46ccf841b6dede77180
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2020-05-13 06:50:31 -0700

    Refactor component loading

--- src/app/component_loaders.js
+++ src/app/component_loaders.js
@@ -13,17 +13,16 @@ export function list_components ()
         /\w+\.js$/       // Filename regular expresion
     );
 
-    const components = [];
+    const components = require_component.keys()
+        .map(file_name =>
+        {
+            const component_config = require_component(file_name);
 
-    require_component.keys().forEach(file_name =>
-    {
-        const component_config = require_component(file_name);
-
-        components.push({
-            name:   filename_without_extension(file_name),
-            config: component_config.default || component_config
+            return {
+                name:   filename_without_extension(file_name),
+                config: component_config.default || component_config
+            };
         });
-    });
 
     return components;
 }
